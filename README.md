94c25ebe04a830de7cd42671c1112150405490a0
Dau The Tien <tiendt2311@gmail.com>
2020-03-12 08:13:06 +0700

    feat(clean_up): clean the code and update read me

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 - Test user
   - username/password: `dthtien/123123`
+- Run test
+  ```
+  bundle exec rspec
+  ```
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,6 @@
 class PagesController < ApplicationController
   def home
     #TODO: fix n + 1
-    @users = User.includes(:employees)
-                .select(:name, :manager_id, :id)
-                .all.map do |user|
-                  UserPresenter.new user
-                end
+    @users = User.includes(:employees).needed_fields
   end
 end
